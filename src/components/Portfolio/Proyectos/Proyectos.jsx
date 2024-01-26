74f5e372caf2b559e316688c9a2806d541896c75
ISRAEL AGUILAR <117057916+isdavid92@users.noreply.github.com>
2024-01-25 21:13:10 -0500

    primer proyecto listo 1

--- src/components/Portfolio/Proyectos/Proyectos.jsx
+++ src/components/Portfolio/Proyectos/Proyectos.jsx
@@ -34,8 +34,20 @@ const Proyectos = () => {
                     <div className={style.encabezado}>
                         <h2 className={style.nombre}>MoveOn</h2>
                         <h3 className={style.fecha}>Sep 2023</h3>
-                        <h5 className={style.descripcion}>Tienda virtual que permite la compra de productos deportivos, suplementos y   proteína.
-                            Cuenta con dos vistas, usuario, y admin, con ambos permite registrarse, comprar, ver los productos, filtrarlos, modificar el perfil. Siendo admin podrá agregar y modificar productos, ver usuarios registrados y actualiza el estado de la compra.</h5>
+                        <h6 className={style.descripcion}>Tienda virtual que permite la compra de productos deportivos, suplementos y   proteína.
+                            Cuenta con dos vistas, usuario, y admin, con ambos permite registrarse, comprar, ver los productos, filtrarlos, modificar el perfil. Siendo admin podrá agregar y modificar productos, ver usuarios registrados y actualiza el estado de la compra.</h6>
+                        <div className={style.aptitudes}>
+                            <img className={style.aptitudImg} src="https://img.shields.io/badge/-JavaScript-05122A?style=flat&logo=javascript" alt="javascript" />
+                            <img className={style.aptitudImg} src="https://img.shields.io/badge/-React-05122A?style=flat&logo=react" alt="react" />
+                            <img className={style.aptitudImg} src="https://img.shields.io/badge/-Node.js-05122A?style=flat&logo=node.js" alt="node.js" />
+                            <img className={style.aptitudImg} src="https://img.shields.io/badge/-Bootstrap-05122A?style=flat&logo=bootstrap&logoColor=563D7C" alt="bootstrap" />
+                            <img className={style.aptitudImg} src="https://img.shields.io/badge/-HTML-05122A?style=flat&logo=HTML5" alt="HTML5" />
+                            <img className={style.aptitudImg} src="https://img.shields.io/badge/-CSS-05122A?style=flat&logo=CSS3&logoColor=1572B6" alt="CSS3" />
+                            <img className={style.aptitudImg} src="https://img.shields.io/badge/-Git-05122A?style=flat&logo=git" alt="HTML5" />
+                            <img className={style.aptitudImg} src="https://img.shields.io/badge/-HTML-05122A?style=flat&logo=HTML5" alt="git" />
+                            <img className={style.aptitudImg} src="https://img.shields.io/badge/-GitHub-05122A?style=flat&logo=github" alt="github" />
+                            <img className={style.aptitudImg} src="https://img.shields.io/badge/-Visual%20Studio%20Code-05122A?style=flat&logo=visual-studio-code&logoColor=007ACC" alt="VS code" />
+                        </div>
                         <a href="https://github.com/fernandoiosono/HENRYPF?tab=readme-ov-file" target="blank" className={style.a}>
                             <p className={style.ver}>{"Ir al repositorio >>"}</p>
                         </a>
